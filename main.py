84a9d99f29a960accef4bdea5b3829c2ae5058f2
Aby Weinberg <abyweinberg@gmail.com>
2021-03-09 01:03:46 +0200

    main.py is now a store of old code

--- main.py
+++ main.py
@@ -30,3 +30,93 @@ print(
 #         break
 #     else:
 #         print(f'The Server Response: {server_response}')
+#  OLD CODE FROM SERVER.PY
+        # try:
+            #     thread = _thread.start_new_thread(my_server.create_new_connection_thread, (conn, addr))
+            #     print(f'The thread is {thread}')
+            # except Exception as e:
+            #     print(f'The exception is : {e}')
+        # conn, addr = threading.Thread(my_server.accept_connection())
+
+        # with conn:
+        #     # print(f'Connected with {addr}')
+        #     while True:
+        #         data = conn.recv(1024)
+        #         print(f'Recived data: {data}')
+        #         if not data:
+        #             break
+        #         conn.sendall(data)
+
+    # def create_new_connection_thread(self, conn, addr):
+    #     print(
+    #         f'connected by {conn} and addr: {addr} from def create new connection thread')
+    #     with conn:
+    #         # print(f'Connected with {addr}')
+    #         while True:
+    #             data = conn.recv(1024)
+    #             print(f'Recived data: {data}')
+    #             if not data:
+    #                 break
+    #             conn.sendall(data)
+
+    # def send_order_to_db(self, querry):
+    #     try:
+    #         cur = self.conn.cursor()
+    #         cur.execute(querry)
+    #         self.conn.commit()
+    #     except Exception as e:
+    #         print(f'Exception: {e}')
+
+# OLD code from client.py
+#    # def create_event_handler(self):
+    #     patterns = '*'
+    #     ignore_patterns = ""
+    #     ignore_directories = True
+    #     case_sensitive = True
+    #     event_handler = PatternMatchingEventHandler(
+    #         patterns, ignore_patterns, ignore_directories, case_sensitive)
+    #     return event_handler
+
+    # def on_deleted(self, event):
+    #     print(f"hey, {event.src_path} has been DELETED!")
+
+    # def on_modified(self, event):
+    #     print(f"hey buddy, {event.src_path} has been modified")
+    #     print(f'event type: {event.event_type}')
+    #     print(f'Event: {event}')
+
+
+   # EVENT HANDLER
+    # my_event_handler = my_client.create_event_handler()
+    # print(f'This is my event handler: {my_event_handler}')
+    # # my_event_handler.on_created = my_client.on_created
+    # my_event_handler.on_modified = my_client.on_modified
+    # go_recursively = False
+    # path_file = '/usr/local/projects/rt-ed/final_projects/python/'
+    # my_observer = Observer()
+    # print(f'This is my observ: {my_observer}')
+    # my_observer.schedule(my_event_handler, path_file, recursive=go_recursively)
+    # my_observer.start()
+    # try:
+    #     while True:
+    #         print('Before sleep 10')
+    #         time.sleep(10)
+    #         print('After sleep 10')
+    # except KeyboardInterrupt:
+    #     my_observer.stop()
+    #     my_observer.join()
+
+    # if my_client.socket.recv(1024):
+    # print(my_client.socket.recv(1024))
+
+    # my_client = Client('2', 'localhost', 12121, file_name)
+    # Check file status
+    # my_client.update_alarm_status()
+    # my_client.update_server()
+    # ------------------------------------- Finish initialization -------------------------------------
+    # print(f'status from my_client: {my_client.file_status}')
+    # new_list = ['2', '0', '0']
+    # if my_client.file_status == new_list:
+    #     print('idem')
+    # else:
+    #     print('not idem')
